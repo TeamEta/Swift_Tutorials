683619e83690c17532ed7304266e3de82bbddb25
Seymour The Llama <aburcham14@georgefox.edu>
2017-02-01 14:57:11 -0800

    More Tutorial Work
    Got through part 3 of the tutorial and started on part 4

--- FoodTracker/FoodTracker/ViewController.swift
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -32,12 +32,36 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         mealNameLabel.text = textField.text
     }
     
+    //MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+        //Dismiss this picker if the user cancels
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
+        
+        
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        // Set photoImageView to display the selected image
+        photoImageView.image = selectedImage
+        
+        // Dismiss the picker
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
     //MARK: Actions
     
     
     
     @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
-        /*// Hide the keyboard
+        // Hide the keyboard
         
         nameTextField.resignFirstResponder()
         
@@ -46,8 +70,14 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         
         // only allow pictures to be picked, not taken
         imagePickerController.sourceType = .photoLibrary
-        */
+ 
+        // set delegate
+        imagePickerController.delegate = self
         
+        // presents view controller defined by imagePickerController, animates presentation of pickerController, and does nothing special after completion
+        present(imagePickerController, animated: true, completion: nil)
+        
+        /*
         // this line checks to see if this device actually has a camera
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
             
@@ -67,7 +97,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
             
             self.present(imagePicker, animated: true, completion: nil)
         }
-        
+        */
     }
     
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
